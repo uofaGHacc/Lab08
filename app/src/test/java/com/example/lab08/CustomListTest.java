18525ef4b19cb0edea3553d36f045a4d30603e91
uofaGHacc <skoh1@ualberta.ca>
2026-02-26 09:20:57 -0700

    Ran failing test for deleteCity.

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -12,4 +12,13 @@ public class CustomListTest {
         // The test should pass now after implementing hasCity() (Step 8)
         assertTrue(list.hasCity(calgary));
     }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+
+        assertTrue(list.deleteCity(calgary));
+    }
 }
